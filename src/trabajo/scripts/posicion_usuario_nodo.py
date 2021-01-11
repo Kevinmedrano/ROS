a9539f863891a14273f572110a47d384a3b9404a
Kmedrano101 <kevin.ejem18@gmail.com>
2021-01-11 17:10:25 +0100

    Fin del Trabajo

--- src/trabajo/scripts/posicion_usuario_nodo.py
+++ src/trabajo/scripts/posicion_usuario_nodo.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
+# Se importa modulos de ROS
 import rospy
 from trabajo.msg import pos_usuario
+# Se importa modulo para TTS
 from espeak import espeak
-espeak.set_voice("es")
 
 def Publicador():
-
+    # Definimos e iniciamos el nodo publicador
     pub = rospy.Publisher('pos_usuario_topic', pos_usuario, queue_size=10)
     rospy.init_node('pos_usuario', anonymous=True)
-    rate = rospy.Rate(1) # 1hz
+    rate = rospy.Rate(1) 
 
     while not rospy.is_shutdown():
-        
+        # Obtenemos los datos de posicion del usuario ingresados por teclado
         pos_usu = pos_usuario()
         rospy.loginfo("Introducir coordenadas de su posicion en X Y Z")
         pos_x = int(raw_input("Ingrese su posicion en X: "))
         pos_y = int(raw_input("Ingrese su posicion en Y: "))
         pos_z = int(raw_input("Ingrese su posicion en Z: "))
-        
+        # Guardamos los valores en el archivo msg
         pos_usu.x = pos_x
         pos_usu.y = pos_y
         pos_usu.z = pos_z
-        
+        # Notificamos y se publican los datos
         rospy.loginfo("Posicion de usuario publicado!")
-        #rospy.loginfo(pos_usu)
         pub.publish(pos_usu)
-        espeak.synth("Se ha enviado la posicion al nodo empaquetador")
         rate.sleep()
 
 if __name__ == '__main__':
--- src/trabajo/scripts/reloj_nodo.py
+++ src/trabajo/scripts/reloj_nodo.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
+# Se importa modulos de ROS
 import rospy
 from std_msgs.msg import String,Bool
 from datetime import datetime
 
-
+# Se crea una clase Reloj
 class Reloj:
+    # Iniciamos la clase con los diferentes valores
     def __init__(self):
         self.subStart_topic = rospy.Subscriber('start_topic', String, self.callbackStart)
         self.subReset_topic = rospy.Subscriber('reset_topic', String, self.callbackReset)
         self.pubStill_topic = rospy.Publisher('still_alive_topic', Bool, queue_size=10)
-        #self.timeUTC = datetime.utcnow()
-        #self.timeLOCAL = datetime.now()
         self.timeLastMessage = datetime.now()
         self.live = True
         self.start = False
         self.reset = False
     
+    # Funcion de callback de start topic
     def callbackStart(self, data):
         rospy.loginfo("Se ha recibido orden de Start para el Reloj!")
         rospy.loginfo(data.data)
         self.timeLastMessage = datetime.now()
         self.start = True
-    
+    # Funcion de callback de reset topic
     def callbackReset(self, data):
         rospy.loginfo("Se ha recibido orden de Reset para el Reloj!")
         rospy.loginfo(data.data)
         timeNow = datetime.now()
+        # Obtenemos el tiempo que demora en llegar el ultimo mensaje
         HH = 0
         MM = 0
         SS = 0
@@ -35,41 +37,39 @@ class Reloj:
             MM = timeNow.minute - self.timeLastMessage.minute
         if(timeNow.second > self.timeLastMessage.second):
             SS = timeNow.second - self.timeLastMessage.second
+        
         cadena = str(HH)+" horas "+str(MM)+" minutos "+str(SS)+" segundos"
         rospy.loginfo("Ultimo mensaje recibido fue hace:")
         rospy.loginfo(cadena)
         self.timeLastMessage = datetime.now()
         self.reset = True
-    
+    # Funcion para obtener la hora UTC y Local
     def getHour_UTC_LOCAL(self, event=None):
         hourUTC = str(datetime.utcnow().hour)+":"+str(datetime.utcnow().minute)+":"+str(datetime.utcnow().second)
         print("UTC Hour: "+hourUTC)
         hourLOCAL = str(datetime.now().hour)+":"+str(datetime.now().minute)+":"+str(datetime.now().second)
         print("LOCAL Hour: "+hourLOCAL)
-
+    # Funcion para obtener el estado del reloj
     def getStateReloj(self):
         self.live = True
         return self.live
-    
+    # Funcion para publicar estado del reloj
     def still_alive(self, event=None):
         print("El Reloj esta funcionando correctamente!")
         msg = Bool()
         msg.data = self.getStateReloj()
         self.pubStill_topic.publish(msg)
 
-
+# Iniciamos el nodo reloj
 rospy.init_node('reloj_nodo', anonymous=True) 
 rospy.loginfo("Nodo Reloj iniciado!")
+# Instanciamos la clase reloj a un objeto
 rl = Reloj()
-#rl.getHour_UTC_LOCAL()
+# Creamos un timer para enviar cada sierto tiempo el estado del reloj
 rospy.Timer(rospy.Duration(1.0/0.1), rl.still_alive)
-#rospy.Timer(rospy.Duration(1.0/1.0), rl.getHour_UTC_LOCAL)
-#rate = rospy.Rate(1)
 
 while not rospy.is_shutdown():
     if rl.start:
+        # Creamos un timer para imprimir la hora UTC y Local actual
         rospy.Timer(rospy.Duration(1.0/1.0), rl.getHour_UTC_LOCAL)
         rl.start = False
-    #rate.sleep()
-
-#rospy.spin()     
